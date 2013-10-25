446d4e575798a51b26804a99a75797f78108bc18
P0wnd <darkday508@hotmail.com>
2013-10-25 02:14:59 +0200

    Base

--- src/com/soulsplit/core/applet/main/MainPanel.java
+++ src/com/soulsplit/core/applet/main/MainPanel.java
@@ -1,5 +1,24 @@
 package com.soulsplit.core.applet.main;
 
-public class MainPanel {
+import java.applet.Applet;
 
+import javax.swing.JFrame;
+
+import com.soulsplit.core.Data;
+import com.soulsplit.core.api.utils.Ini;
+import com.soulsplit.loader.Main;
+
+public class MainPanel extends JFrame{
+	JFrame frame = new JFrame(Data.APP_NAME + " " + Data.CODENAME + " " + Ini.getVersion());
+	Applet applet = new Applet();
+	
+	public MainPanel(){
+		frame.setSize(getMaximumSize());
+		frame.setVisible(true);
+		
+		frame.add(applet);
+		
+
+		
+	}
 }
